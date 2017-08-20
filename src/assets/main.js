2e2f2c92612c02cbad53ff639d991042d524be96
Yuriy Zubaha <yuzub@ua.fm>
2017-08-21 02:40:31 +0300

    Enf of satisfying the list of requirements

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,34 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/Yuriy_Zubaha.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      var $badges = $('#badges');
+      var courses = response.courses.completed;
+      courses.forEach(function(course) {
+        $div = $('<div />', {
+          'class': 'course'
+        }).appendTo($badges);
+
+        $('<h3 />', {
+          text: course.title
+        }).appendTo($div);
+
+        $('<img />', {
+          src: course.badge
+        }).appendTo($div);
+
+        $('<a />', {
+          'class': 'btn btn-primary',
+          target: '_blank',
+          href: course.url,
+          text: 'See Course'
+        }).appendTo($div);
+      });
+    }
+  });
 
 });
